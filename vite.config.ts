0e7ef679bd938708158d952c364010f1d375ed5d
Arpit <ak30062004@gmail.com>
2025-09-12 12:46:55 +0530

    Minor deplayement chnage

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,17 @@ import react from '@vitejs/plugin-react-swc'
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 
-// https://vite.dev/config/
 export default defineConfig({
-   plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    historyApiFallback: true,
+  },
+  preview: {
+    historyApiFallback: true,
   },
 })
